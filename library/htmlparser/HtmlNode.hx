45e328c4fe285a313649f103c5acdd2545904d30
Yaroslav Sivakov <yar3333@gmail.com>
2017-08-25 01:06:44 +0300

    + init

---
+++ library/htmlparser/HtmlNode.hx
@@ -0,0 +1,13 @@
+package htmlparser;
+
+@:jsRequire("re-htmlparser", "HtmlNode") extern class HtmlNode
+{
+	var parent : htmlparser.HtmlNodeElement;
+	function remove() : Void;
+	function getPrevSiblingNode() : htmlparser.HtmlNode;
+	function getNextSiblingNode() : htmlparser.HtmlNode;
+	function toString() : String;
+	function toText() : String;
+	private function hxSerialize(s:{ function serialize(d:Dynamic) : Void; }) : Void;
+	private function hxUnserialize(s:{ function unserialize() : Dynamic; }) : Void;
+}
---
+++ library/htmlparser/XmlDocument.hx
@@ -0,0 +1,6 @@
+package htmlparser;
+
+@:jsRequire("re-htmlparser", "XmlDocument") extern class XmlDocument extends htmlparser.XmlNodeElement
+{
+	function new(?str:String) : Void;
+}
