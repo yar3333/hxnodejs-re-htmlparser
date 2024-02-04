d8ab4c5ef3ea761043137cce696ed625b0f8af14
Yaroslav Sivakov <yar3333@gmail.com>
2024-02-04 17:50:11 +0300

    * minor

--- example-typescript/src/index.ts
+++ example-typescript/src/index.ts
@@ -4,5 +4,9 @@ var doc = new HtmlDocument("<a>InA</a>");
 
 console.log("Test!");
 console.log(doc.toString());
+console.log(doc.innerHTML);
+console.log(doc.children.length);
+console.log(doc.children[0].name);
+doc.innerHTML = "<b>InB</b>";
 console.log(doc.children.length);
 console.log(doc.children[0].name);
